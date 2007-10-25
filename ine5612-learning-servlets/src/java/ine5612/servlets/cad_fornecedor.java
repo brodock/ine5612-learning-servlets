6400f51cd00f529fce25df19a044a194d51242bd
ramonfacchin <ramonfacchin@0a7d99a4-a73c-0410-a022-171e34bb9763>
2007-10-25 15:09:33 +0000

    criadas classes para tratar o cadastro com serialização
    classe FornecedorLista está incompleta, só possui os métodos para carregar/salvar arquivo, falta implementar o tratamento das diferentes buscas

--- ine5612-learning-servlets/src/java/ine5612/servlets/cad_fornecedor.java
+++ ine5612-learning-servlets/src/java/ine5612/servlets/cad_fornecedor.java
@@ -45,6 +45,46 @@ public class cad_fornecedor extends HttpServlet {
             out.close();
         }
     }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet cad_fornecedor</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet cad_fornecedor at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+             */
+        } finally {
+            out.close();
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet cad_fornecedor</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet cad_fornecedor at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+             */
+        } finally {
+            out.close();
+        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -54,23 +94,7 @@ public class cad_fornecedor extends HttpServlet {
      * @throws javax.servlet.ServletException
      * @throws java.io.IOException
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws javax.servlet.ServletException
-     * @throws java.io.IOException
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
+    
     /**
      * Returns a short description of the servlet.
      * @return
